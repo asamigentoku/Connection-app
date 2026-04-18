f8d4e3c4db6c7cab7e511a932a70b07125988477
asamigentoku <gentoku.asami.9v@stu.hosei.ac.jp>
2026-04-18 14:40:10 +0900

    アプリの大体が完成

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -11,10 +11,11 @@ import {
     TweetApi,
     FriendsApi
 } from "../api/apis";
+const BASE_PATH = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000";
 
 // 共通設定（JWTなどもここに集約できる）
 const config = new Configuration({
-    basePath: "http://localhost:8000",
+    basePath: BASE_PATH,
     accessToken: () => {
         const token = localStorage.getItem("accessToken"); // ←ここ統一
         return token ? `Bearer ${token}` : "";
